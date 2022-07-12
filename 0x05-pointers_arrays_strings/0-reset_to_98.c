513e17178dfbda17be036314f048287fc71a0f86
eddienyte <eddynyte@gmail.com>
2022-07-12 05:41:14 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,11 @@
 
 /**
  * reset_to_98 -> a function to update the reference of a pointer
- * @n: pointer to n to be updated
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
